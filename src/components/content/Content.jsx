666172a094cc8455bcbdbc61b69921fb51647325
Shevchuk-Vadym <lorelliand@gmail.com>
2022-05-02 15:39:35 +0300

    add search

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Images} from '../Images';
-import { render } from '@testing-library/react';
+import { Images } from '../Images';
+import { getValue } from '@testing-library/user-event/dist/utils';
+
 const ACCES_KEY = process.env.REACT_APP_ACCES_KEY;
 
 const Content = () => {
+  // const search = document.getElementById('unsplash').value;
   const [images, setImages] = useState([]);
+  const [inputValue, setInputValue] = useState();
+  const searchPhoto = async () => {
+    const response = await axios.get(
+      'https://api.unsplash.com/search/photos?client_id=' +
+        ACCES_KEY +
+        '&query=' +
+        inputValue
+    );
+    const data = await response.data;
+    setImages(data.results);
+    console.log(images);
+  };
 
   const fetchAPI = async () => {
     const response = await axios.get(
@@ -17,12 +31,18 @@ const Content = () => {
   };
   useEffect(() => {
     fetchAPI();
-    console.log('useEffect');
   }, []);
-  console.log('RENDER');
+
   return (
     <div>
-      {/* <button onClick={fetchAPI}> CLICK ME!</button> */}
+      <input
+        type='text'
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder='search photo'
+        id='unsplash'
+      />
+      <button onClick={searchPhoto}> CLICK ME!</button>
       <Images images={images} />
     </div>
   );
